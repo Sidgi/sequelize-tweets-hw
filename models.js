334bab743516889d32d8b8606ddafa73271ebd75
Sidgi Samanduyev <samanduyev@gmail.com>
2019-05-01 14:17:27 -0400

    initial commit

--- models.js
+++ models.js
@@ -9,6 +9,15 @@ const sequelize = new Sequelize({
   }
 });
 
+const Tweets = sequelize.define('tweet',{
+  text: Sequelize.STRING,
+  hashtag: Sequelize.STRING,
+  retweets: Sequelize.INTEGER
+})
+module.exports = {
+  sequelize,
+  Tweets
+}
 //create your model here:
 //1. Write a sequelize model for tweets. It should include columns for
 // -text (string)
